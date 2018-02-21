a543aeedeeae79d521433ef48344e5c533cb945b
happyandhappy <greyfrapps@gmail.com>
2018-02-21 16:21:06 +0800

    second

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +14,60 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  root:any = HomePage;
+  constructor(public actionSheetCtrl: ActionSheetController) {}
+  setRootPage(tab:any){
+    switch(tab){
+      case 'tab1Root':
+        this.root = HomePage;
+        break;
+      case 'tab2Root':
+        this.root = AboutPage;
+        break;
+      case 'tab3Root':
+        this.root = ContactPage;
+        break;
+      case 'tab4Root':
 
+        break;
+    }
+  }
+  presentActionSheet() {
+    console.log("more clicked");
+    let actionSheet = this.actionSheetCtrl.create({
+      // title: 'Modify your album',
+      buttons: [
+        {
+          text: 'LogOut',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Settings',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'About',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Discover',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 }
+
